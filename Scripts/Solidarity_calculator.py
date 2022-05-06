61c759d177a0a69d492ec59ab741a2c53285d717
Kristian Smedsrød <29783486+krissmed@users.noreply.github.com>
2022-05-06 22:49:21 +0200

    Delete Solidarity_calculator.py

--- Scripts/Solidarity_calculator.py
+++
@@ -1,3 +0,0 @@
-# When a transfer has a transfer fee, the solidarity contributuion should be calculated
-# This can be used: https://www.transfermarkt.co.uk/solidarityFeeCalculator
-# The best would be our own implementation based on FM data
