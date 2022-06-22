ba8a421e35f16e6358652cc2d56b8e1c8d23a5d2
Kristian Smedsrød <29783486+krissmed@users.noreply.github.com>
2022-06-22 16:52:24 +0200

    WIP: Initalize_new_transfer_script.py is done. Needs some more testing

--- Scripts/Initalize_new_transfer_script.py
+++ Scripts/Initalize_new_transfer_script.py
@@ -4,8 +4,6 @@ import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from random import randint
-from time import sleep
 
 def create_bs4_object(url):
     html_content = requests.get(url, headers={
@@ -17,7 +15,11 @@ def create_bs4_object(url):
 
 def get_competition_urls():
     print('[PROCESSING] Building list of all competitions')
-    competition_urls = ['https://www.transfermarkt.com/wettbewerbe/europa/wettbewerbe', 'https://www.transfermarkt.com/wettbewerbe/asien', 'https://www.transfermarkt.com/wettbewerbe/amerika', 'https://www.transfermarkt.com/wettbewerbe/afrika']
+    competition_urls = ['https://www.transfermarkt.com/wettbewerbe/europa/wettbewerbe',
+                        'https://www.transfermarkt.com/wettbewerbe/asien',
+                        'https://www.transfermarkt.com/wettbewerbe/amerika',
+                        'https://www.transfermarkt.com/wettbewerbe/afrika'
+                        ]
     transfer_urls = []
     for url in competition_urls:
         page_number = 1
@@ -44,6 +46,14 @@ def get_competition_urls():
     return transfer_urls
 
 
+def check_if_transfer(soup):
+    is_transfer = False
+    for subnav in soup.find_all("a", {"class": "tm-subnav-item megamenu_drop"}):
+        if subnav.text == "Transfers":
+            is_transfer = True
+    return is_transfer
+
+
 def add_to_urllist(player_object):
     link = f"https://www.transfermarkt.com{player_object['href']}/plus/1"
     link = link.replace("startseite", "letztetransfers")
@@ -66,103 +76,88 @@ def make_list_of_players(comp_urls):
     for url in comp_urls:
         print(f"[PROCESSING] Building list of all transferred players {counter}/{len(comp_urls)}")
         soup = create_bs4_object(url)
-        player_append = [url]
-        if not check_if_transfer(soup):
-            continue
         has_run = False
-        for player in soup.find_all('tr', {'class': 'odd'}):
+        i = 0
+        if check_if_transfer(soup) is False:
+            continue
+        print(f"[Processing] Running on url: {url}")
+        for player_odd, player_even in zip(soup.find_all('tr', {'class': 'odd'}), soup.find_all('tr', {'class': 'even'})):
+            print(player_odd.find('img', {'class': 'bilderrahmen-fixed'})['alt'])
             if has_run is False:
-                player_append = extract(player)
-                print(player_append)
-                player_append.insert(0, url)
-                first_transfers.append(player_append)
-                print(first_transfers)
-                print(f"url: {url}")
-            #check_last_transfer(url, player)
-            #player_list.append(player)
+                new_first_transfer = extract(player_odd, url)
+                new_first_transfer.insert(0, url)
+                first_transfers.append(new_first_transfer)
+                has_run = True
+                print(new_first_transfer)
+            # player_extract = extract(player_odd, url)
+            # print(player_extract)
+            # if not player_extract:
+            #     print("[Player_extract] Breaking")
+            #     break
+            # player_list.append(player_extract)
             # print(f"[PROCESSING] Added player to list (Total: {len(player_list)})")
-            has_run = True
-        #for player in soup.find_all('tr', {'class': 'even'}):
-            #player_list.append(player)
+            # print(player_even.find('img', {'class': 'bilderrahmen-fixed'})['alt'])
+            # player_extract = extract(player_even, url)
+            # if not player_extract:
+            #     print("[Player_extract] Breaking")
+            #     break
+            # player_list.append(player_extract)
             # print(f"[PROCESSING] Added player to list (Total: {len(player_list)})")
+            i += 1
         counter += 1
-    # print(f'[PROCESSING] Found {len(player_list)} players')
-    return first_transfers
+    print(f'[PROCESSING] Found {len(player_list)} players')
+    return first_transfers, player_list
 
 
-def check_if_transfer(soup):
-    is_transfer = False
-    for subnav in soup.find_all("a", {"class": "tm-subnav-item megamenu_drop"}):
-        if subnav.text == "Transfers":
-            is_transfer = True
-    return is_transfer
+def extract(player, url):
 
-
-def check_last_transfer(url, player):
-    # if LAST_TRANSFER == player:
-    #    return False
-    pass
-
-
-def extract_data(players):  # Returns a dataframe of all players
-    data = []
-    counter = 1
-    for player in players:
-        print(f"Player {counter}/{len(players)}")
-        new_data = extract(player)
-        counter += 1
-
-
-def extract(player):
     name = player.find('img', {'class': 'bilderrahmen-fixed'})['alt']
-    print(f"name: {name}")
+
     tfm_player_id = player.find('a', {'title': name})['href'].split('/')[4]
-    print(f"tfm_player_id: {tfm_player_id}")
-    # position = player.find_all('td', {'class': ''})[2].text
-    age = player.find_all('td', {'class': 'zentriert'})[1].text
-    print(f"age: {age}")
-    fir_nat = player.find_all('img', {'class': 'flaggenrahmen'})[0]['title']
-    print(f"fir_nat: {fir_nat}")
-    if len(player.find_all('img', {'class': 'flaggenrahmen'})) > 3:
-        sec_nat = player.find_all('img', {'class': 'flaggenrahmen'})[1]['title']
+
+    club_to = player.find_all('img', {'class': 'tiny_wappen'})[1]['alt']
+
+    if club_to == "Retired" or club_to == "Career break":
+        club_to_tfm_id = "Null"
     else:
-        sec_nat = "Null"
-    print(f"sec_nat: {sec_nat}")
+        club_to_tfm_id = player.find('a', {'title': club_to})['href'].split('/')[4]
+
     club_from = player.find('img', {'class': 'tiny_wappen'})['alt']
-    print(f"club_from: {club_from}")
+
     if club_from == "Retired" or club_from == "Career break":
         club_from_tfm_id = "Null"
     else:
         club_from_tfm_id = player.find('a', {'title': club_from})[
             'href'].split('/')[4]
-    print(f"club_from_tfm_id: {club_from_tfm_id}")
-    club_to = player.find_all('img', {'class': 'tiny_wappen'})[1]['alt']
-    print(f"club_to: {club_to}")
-    if club_to == "Retired" or club_to == "Career break":
-        club_to_tfm_id = "Null"
-    else:
-        club_to_tfm_id = player.find('a', {'title': club_to})['href'].split('/')[4]
-    print(f"club_to_tfm_id: {club_to_tfm_id}")
+
     transfer_date = player.find_all('td', {'class': 'zentriert'})[2].text
-    print(f"transfer_date: {transfer_date}")
+
+    if not compare_last_transfer(url, tfm_player_id, club_from_tfm_id, club_to_tfm_id, transfer_date):
+        print("[ERROR]Transfer already listed")
+        return False
+
+    age = player.find_all('td', {'class': 'zentriert'})[1].text
+
+    fir_nat = player.find_all('img', {'class': 'flaggenrahmen'})[0]['title']
+
+    if len(player.find_all('img', {'class': 'flaggenrahmen'})) > 3:
+        sec_nat = player.find_all('img', {'class': 'flaggenrahmen'})[1]['title']
+    else:
+        sec_nat = "Null"
+
     transfer_type = player.find_all('td', {'class': 'rechts hauptlink'})[0].text
 
     position, date_joined, contract_length, full_name = getting_player_details(
         player, name)
-    print(f"position, date_joined, contract_length, full_name: {position, date_joined, contract_length, full_name}")
-    return [tfm_player_id, name, full_name, position, age, fir_nat, sec_nat, club_from, club_from_tfm_id, club_to, club_to_tfm_id, transfer_date, transfer_type, date_joined.strip(), contract_length]
+
+    return[tfm_player_id, name, full_name, position, age, fir_nat, sec_nat, club_from, club_from_tfm_id, club_to,
+           club_to_tfm_id, transfer_date, transfer_type, date_joined.strip(), contract_length.strip()]
 
 
 def getting_player_details(player, name):
-    # Adding transfermarkt id to the table
-    # URL to player profile
-    url = (
-        f"http://www.transfermarkt.com{player.find('a', {'title' : name})['href']}")
-    html_content = requests.get(url, headers={
-                                'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}).text
-    # Parse the html content
-    soup = BeautifulSoup(html_content, "lxml")
-    player_data = soup.find('div', {'class': ['info-table info-table--right-space', 'info-table info-table--right-space min-height-audio']}).find_all('span')  # Finds all player data
+    soup = create_bs4_object("https://www.transfermarkt.com"+player.find('a', {"title": name})['href'])
+    player_data = soup.find('div', {'class': ['info-table info-table--right-space',
+                                              'info-table info-table--right-space min-height-audio']}).find_all('span')
 
     counter = 0  # Reset the counter to 0
     full_name = "Null"
@@ -183,29 +178,47 @@ def getting_player_details(player, name):
     return position.strip(), date_joined.strip(), contract_length, full_name
 
 
-def get_last_transfer(player_id, clubFrom_id, clubTo_id):
-    global last_transfer
+def compare_last_transfer(url, player_id, club_from_id, club_to_id, transfer_date):
+    if last_transfer is not False:
+        for i in range(len(last_transfer)):
+            if last_transfer[i] == [url, player_id, club_from_id, club_to_id, transfer_date]:
+                print("[ERROR]Transfer already listed")
+                print(last_transfer)
+                last_transfer.pop(i)
+                return False
+    return True
 
 
-def debug(string):
-    return f"[DEBUG] {string}"
+def get_last_transfer():
+    if os.path.isfile('../Output/Latest_transfersa.json'):  # Checks if the file exists
+        f = open('../Output/Latest_transfersa.json')
+        data = json.load(f)
+        last_transfers = []
+        for item in range(len(data)):
+            last_transfers.append([data[str(item)]['Url'], data[str(item)]['Player_id'], data[str(item)]['From club id'], data[str(item)]['To club id'], data[str(item)]['Tranfer date']])
+            print(len(last_transfers))
+        return last_transfers
+    else:
+        return False
 
 
 def export_data(df):  # Export to json or csv
     print(12342)
     if not os.path.exists("../Output"):
         os.mkdir("../Output")
-    df.to_csv('../Output/Latest_transfersa.csv', index=False)
+    df.to_csv('../Output/latest_transfers.csv', index=False)
 
-    df.to_json('../Output/Latest_transfersa.json', orient="records")
-
+    df.to_json('../Output/latest_transfers.json', orient="records")
 
 
 if __name__ == "__main__":
-    #last_transfer = get_last_id()
+    global last_transfer
+    last_transfer = get_last_transfer()
     comp_urls = get_competition_urls()
-    player_list = make_list_of_players(comp_urls)
-    print(len(player_list[0]))
-    df = pd.DataFrame(data=player_list, columns=["Url", "Player_id", "Name", "Full Name", "Position", "Age", "Nationality", "Second Nationality", "From club", "From club id", "To club", "To club id", "Tranfer date", "Fee", "Date joined", "Contract expiry date"])
-    print(df)
+    first_transfer, player_list = make_list_of_players(comp_urls)
+    print(first_transfer)
+    df = pd.DataFrame(data=first_transfer,
+                      columns=["Url", "Player_id", "Name", "Full Name", "Position", "Age", "Nationality",
+                               "Second Nationality", "From club", "From club id", "To club", "To club id",
+                               "Tranfer date", "Fee", "Date joined", "Contract expiry date"])
     export_data(df)
